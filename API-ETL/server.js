5dafede99472a2ae960b984288585c73c2ae95bf
david-baca <david.baca.ingenieria@egmail.com>
2024-07-28 10:15:02 -0500

    #parche eliminacion para desarrollo en el area de front

--- API-ETL/server.js
+++ API-ETL/server.js
@@ -1,28 +1,7 @@
 const app = require('./app');
-const http = require('http');
-const { Server } = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3700",
-        methods: ["GET", "POST"]
-    }
-})
-
-io.on('connection', (socket) => {
-    console.log(`A user connected: ${socket}`)
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 }); 
-
-module.exports = io;
